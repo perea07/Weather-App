25b521ce857590ae7c76c4ccaa4363f29bb529dd
Yeison Perea <ye.perea09@ciaf.edu.co>
2024-03-26 11:05:35 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// //const apiUrl = "";
+// //const apiUrl = ''
+// const apiUrl =
+//   "https://";
+// fetch(`${apiUrl}`)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then((apiResponse) => {
+//     console.log(apiResponse);
+//     // console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+//   })
+//   .catch((error) => {
+//     console.error("There has been a problem with your fetch operation:", error);
+//   });
